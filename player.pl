c46081464a0bfbe89082186834de60b1362205e6
Zeus-s <kristabdixx@gmail.com>
2021-11-22 15:08:31 +0700

    revamp and debug player.pl

--- player.pl
+++ player.pl
@@ -10,22 +10,6 @@
 max_level(10).
 game_start(0).
 
-/* Jenis job*/
-playerChoose(1) :-
-    write('You choose Farmer'),
-    assertz(player(1, 1, 1, 1, 0, 0, 0, 0).
-    asserta(game_start(1)).
-
-playerChoose(2) :-
-    write('You choose Fisherman'),
-    assertz(player(2, 1, 1, 1, 0, 0, 0, 0).
-    asserta(game_start(1)).
-
-playerChoose(3) :-
-    write('You choose Rancher'),
-    assertz(player(3, 1, 1, 1, 0, 0, 0, 0).
-    asserta(game_start(1)).
-
 /* Player dan Level Awal */
 player(1, 1, 1, 1, 1, 0, 0, 0, 0).
 /* Job, Level, Farming Level, Fisherman Level, Ranching Level, EXP, EXP Farming, EXP FIshing, EXP Ranching */
@@ -33,6 +17,28 @@ player(1, 1, 1, 1, 1, 0, 0, 0, 0).
         2. Fisherman
         3. Ranching
 */
+
+/* Jenis job*/
+playerChoose(1) :-
+    write('You choose Farmer'),
+    retract(player(_, _, _, _, _, _, _, _, _)),
+    retract(game_start(_)),
+    asserta(player(1, 1, 1, 1, 0, 0, 0, 0)),
+    asserta(game_start(1)).
+
+playerChoose(2) :-
+    write('You choose Fisherman'),
+    retract(player(_, _, _, _, _, _, _, _, _)),
+    retract(game_start(_)),
+    asserta(player(2, 1, 1, 1, 0, 0, 0, 0)),
+    asserta(game_start(1)).
+
+playerChoose(3) :-
+    write('You choose Rancher'),
+    retract(player(_, _, _, _, _, _, _, _, _)),
+    retract(game_start(_)),
+    asserta(player(3, 1, 1, 1, 0, 0, 0, 0)),
+    asserta(game_start(1)).
 
 addLevel:-
     write('                                               \n'),
@@ -48,122 +54,84 @@ addLevel:-
     retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
     asserta(player(Role, Level_Now, Level_Now, Level_Now, Level_Now, 0, 0, 0, 0)).
 
-/%
-addFarmLevel:-
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('             █░░ █▀▀ █░█ █▀▀ █░░   █░█ █▀█ █   \n'),
-    write('             █▄▄ ██▄ ▀▄▀ ██▄ █▄▄   █▄█ █▀▀ ▄   \n'),
-    write('                                               \n'),
-    player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch),
-    Level_Now is Level + 1,
-    retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
-    asserta(player(Role, Level, Level_Now, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)), !.
-
-addFishLevel:-
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('             █░░ █▀▀ █░█ █▀▀ █░░   █░█ █▀█ █   \n'),
-    write('             █▄▄ ██▄ ▀▄▀ ██▄ █▄▄   █▄█ █▀▀ ▄   \n'),
-    write('                                               \n'),
-    player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch),
-    Level_Now is Level + 1,
-    retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
-    asserta(player(Role, Level, FarmLevel, Level_Now, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)), !.
-
-addRanchLevel:-
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('                                               \n'),
-    write('             █░░ █▀▀ █░█ █▀▀ █░░   █░█ █▀█ █   \n'),
-    write('             █▄▄ ██▄ ▀▄▀ ██▄ █▄▄   █▄█ █▀▀ ▄   \n'),
-    write('                                               \n'),
-    player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch),
-    Level_Now is Level + 1,
-    retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
-    asserta(player(Role, Level, FarmLevel, FishLevel, Level_Now, EXP, EXPFarm, EXPFish, EXPRanch)), !.
-%/
 
 addExp(X) :-
     player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch),
-    Role -:- 1, !,
-    X = 1 -> (
+    Role =:= 1, !,
+    (
+    X =:= 1 -> (
         retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPFarm is EXPFarm + 6,
         NewEXP is (NewEXPFarm + EXPFish + EXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, NewEXPFarm, EXPFish, EXPRanch)),checkLevelUp(_),!.
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, NewEXPFarm, EXPFish, EXPRanch)),checkLevelUp(_),!
     )
     ;
-    X = 2 -> (
+    X =:= 2 -> (
         retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPFish is EXPFish + 3,
         NewEXP is (EXPFarm + NewEXPFish + EXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, NewEXPFish, EXPRanch)),checkLevelUp(_),!.
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, NewEXPFish, EXPRanch)),checkLevelUp(_),!
     )
     ;
-    (
-       retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
+    X =:= 3 -> (
+        retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPRanch is EXPRanch + 3,
         NewEXP is (EXPFarm + EXPFish + NewEXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, EXPFish, NewEXPRanch)),checkLevelUp(_),!. 
-    ).
-    
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, EXPFish, NewEXPRanch)),checkLevelUp(_),!
+    )
+).
 
 addExp(X) :-
     player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch),
-    Role -:- 2, !,
-    X = 1 -> (
+    Role =:= 2, !,
+    (
+    X =:= 1 -> (
         retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPFarm is EXPFarm + 3,
         NewEXP is (NewEXPFarm + EXPFish + EXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, NewEXPFarm, EXPFish, EXPRanch)),checkLevelUp(_),!.
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, NewEXPFarm, EXPFish, EXPRanch)),checkLevelUp(_),!
     )
     ;
-    X = 2 -> (
+    X =:= 2 -> (
         retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPFish is EXPFish + 6,
         NewEXP is (EXPFarm + NewEXPFish + EXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, NewEXPFish, EXPRanch)),checkLevelUp(_),!.
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, NewEXPFish, EXPRanch)),checkLevelUp(_),!
     )
     ;
-    (
-       retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
+    X =:= 3 -> (
+        retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPRanch is EXPRanch + 3,
         NewEXP is (EXPFarm + EXPFish + NewEXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, EXPFish, NewEXPRanch)),checkLevelUp(_),!. 
-    ).
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, EXPFish, NewEXPRanch)),checkLevelUp(_),!
+    )
+).
 
 addExp(X) :-
     player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch),
-    Role -:- 3, !,
-    X = 1 -> (
+    Role =:= 3, !,
+    (
+    X =:= 1 -> (
         retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPFarm is EXPFarm + 3,
         NewEXP is (NewEXPFarm + EXPFish + EXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, NewEXPFarm, EXPFish, EXPRanch)),checkLevelUp(_),!.
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, NewEXPFarm, EXPFish, EXPRanch)),checkLevelUp(_),!
     )
     ;
-    X = 2 -> (
+    X =:= 2 -> (
         retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPFish is EXPFish + 3,
         NewEXP is (EXPFarm + NewEXPFish + EXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, NewEXPFish, EXPRanch)),checkLevelUp(_),!.
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, NewEXPFish, EXPRanch)),checkLevelUp(_),!
     )
     ;
-    (
-       retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
+    X =:= 3 -> (
+        retract(player(Role, Level, FarmLevel, FishLevel, RanchLevel, EXP, EXPFarm, EXPFish, EXPRanch)),
         NewEXPRanch is EXPRanch + 6,
         NewEXP is (EXPFarm + EXPFish + NewEXPRanch)/3,
-        asserta(player(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, EXPFish, NewEXPRanch)),checkLevelUp(_),!. 
-    ).
+        asserta(player(Role, Level, FarmLevel, FishLevel, RanchLevel, NewEXP, EXPFarm, EXPFish, NewEXPRanch)),checkLevelUp(_),!
+    )
+).
 
 checkLevelUp(0) :-
     player(_, Level, _, _, _, EXP, _, _, _),
@@ -175,17 +143,17 @@ checkLevelUp(1) :-
     addLevel, !.
 
 writejob(X) :-
-    X -:- 1, !,
+    X=:=1, !,
     write('Farmer').
 
 
 writejob(X) :-
-    X -:- 2, !,
+    X =:= 2, !,
     write('Fisherman').
 
 
 writejob(X) :-
-    X -:- 3, !,
+    X =:= 3, !,
     write('Rancher').
 
 
