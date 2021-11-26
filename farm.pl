f0c628844c20f97828a7033d700e584065a740c0
YakobusIP <16520120@std.stei.itb.ac.id>
2021-11-26 20:46:28 +0700

    fix digging and planting

--- farm.pl
+++ farm.pl
@@ -1,6 +1,7 @@
 /* File : farm.pl */
 /* Store farming information */
 :- dynamic(farm/4).
+:- dynamic(counter/1).
 
 /* Format tanaman: plant(Type, GrowthDuration, BuyPrice, SellPrice) */
 /* Type 1=kentang, 2=labu, 3=tomat, 4=semangka, 5=brokoli, 6=anggur, 7=gandum, 8=jagung, 9=nanas, 10=durian*/
@@ -19,9 +20,48 @@ plant(10, 15, 1250, 5000).
 farm(1, 8,1,2).
 farm(2, 7,3,4).
 
-dig:-(
-  write('You digged the tile')
-).
+/* Dig ground */
+counter(1).
+
+dig:-
+    interiorObject(Player_X, Player_Y, 'P'),
+    counter(CurrentCounter),
+    tilledGround(CurrentCounter, Current_X, Current_Y, _),
+
+    % Fill tilledGround facts
+    (
+    Current_X =:= 0, Current_Y =:= 0, CurrentCounter =< 15 -> (
+        % Check location to see if there is already digged ground there
+        (
+        tilledGround(_, Player_X, Player_Y, _) -> (
+            write('You already dugged a land here! Move somewhere else!'), !   
+        );
+        (
+            retract(tilledGround(_, _, _, _)),
+            assertz(tilledGround(CurrentCounter, Player_X, Player_Y, 0))
+        )
+        )
+        );    
+    Current_X > 0, Current_Y > 0 -> (
+        NextCounter is CurrentCounter + 1,
+        retract(counter(_)),
+        assertz(counter(NextCounter)),
+        dig
+        )    
+    ).
+
+dig:-
+    write('You cannot dig anymore ground!').
+
+/* PROTOTYPE FUNGSI PLANT */
+plant:-
+    write('Which plant?'), nl,
+    read(Plant),
+    interiorObject(Player_X, Player_Y, 'P'),
+    tilledGround(Counter, Player_X, Player_Y, _),
+    retract(tilledGround(Counter, _, _, _)),
+    assertz(tilledGround(Counter, Player_X, Player_Y, Plant)),
+    write('Successfully planted!').
 
 /* TODO samain kaya yang di inventory, tambahin exp, bikin fungsi dig */
 farm:-(
