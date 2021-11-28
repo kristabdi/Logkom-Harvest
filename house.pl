d79068a04aa50dc34efd8b0574e00e60dfb9ede7
YakobusIP <16520120@std.stei.itb.ac.id>
2021-11-28 20:40:47 +0700

    fix house.pl

--- house.pl
+++ house.pl
@@ -1,25 +1,4 @@
-:- dynamic(save_counter/1).
-:- dynamic(save_list/1).
-/* List Data Type */
-/* InsertAt */
-addTo([],X,X):- !.
-addTo([A|B],C,[A|D]):-
-    addTo(B,C,D).
-
-insertAt(L,E,1,[E|L]).
-insertAt([H|T],E,P,Result):-
-    Next is P-1,
-    insertAt(T,E,Next,Temp),
-    addTo([H],Temp,Result).
-
-/* Delete Element */
-deleteElmt(Element, [Element|Tail], Tail).
-deleteElmt(Element, [Head|Tail], [Head|NextTail]):-
-    deleteElmt(Element, Tail, NextTail).
-
 /* House */
-save_list([]).
-save_counter(1).
 house:-
     write('What do you wish to do here?'), nl,
     write('1. Sleep'), nl,
